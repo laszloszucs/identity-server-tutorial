72f84c0819bf797564f0b501e6fb8103bb0a27e1
László Szücs <laszlo.szucs@euroknows.com>
2020-03-02 18:35:43 +0100

    Add IdentityServer project files.

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,52 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        /// <summary>
+        /// Ez mondja meg milyen API-kat érhetnek el
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new[]
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        /// <summary>
+        /// Ez mondja meg milyen alkalmazások csatlakozhatnak és azok melyik API-kat érhetik el
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients()
+        {
+            return new [] { new Client
+            {
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            }};
+        }
+    }
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IHostingEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.GetIdentityResources()) // TODO http://localhost:5000/.well-known/openid-configuration
+                .AddInMemoryApiResources(Config.GetApis()) // Itt töltődnek be az Resource-ok (API-k, amiket védeni kell)
+                .AddInMemoryClients(Config.GetClients()); // és a Client-ek, melyek a megbízható alkalmazások
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential(); // developer signing key generálás, nem kell verziókezelni, mindig létrejön magától, hogyha nincs
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+        }
+    }
+}
