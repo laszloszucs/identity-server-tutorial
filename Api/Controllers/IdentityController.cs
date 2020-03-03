fe66539a2908a0f67972e386518ca96ca691dc45
László Szücs <laszlo.szucs@euroknows.com>
2020-03-03 09:15:03 +0100

    Api Project egy Controllerrel és egy Actionnel (get végpont)

---
+++ Api/Controllers/IdentityController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize] // TODO Ha meghívom a böngészőben a localhost:5001/api/identity-t, akkor 401-et kell kapni (Unauthorized) -> Működik az API protection
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(
+                User.Claims.Select(claim => new
+                {
+                    claim.Type,
+                    claim.Value
+                }));
+        }
+    }
+}
---
+++ Api/Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Api
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                .AddNewtonsoftJson()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            /*
+             AddAuthentication:
+                 Autentikációs service-t hozzáadja a DI-hez és beállítja a default sémát "Bearer"-re.
+                 Továbbá beállít egy Middleware-t, hogy az Autentikáció automatikusan megtörténjen minden API híváskor
+            */
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+
+                    options.Audience = "api1";
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
