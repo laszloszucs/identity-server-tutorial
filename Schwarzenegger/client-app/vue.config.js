c041d2d7aad0aca4d916f5f99f8e7005d74e5c39
László Szücs <laszlo.szucs@euroknows.com>
2020-03-10 09:52:00 +0100

    .gitignore, Vue CLI, package.json konfigok

--- Schwarzenegger/client-app/vue.config.js
+++ Schwarzenegger/client-app/vue.config.js
@@ -1,4 +1,6 @@
 ﻿module.exports = {
-    outputDir: "../wwwroot/client-app/", // ASP.NET Core public folder
-    filenameHashing: false // I'll let ASP.NET Core handle cache breaking
+    devServer: { https: true, host: 'localhost', port: 44301, public: 'https://localhost:44301' } ,
+    outputDir: "../wwwroot/", // /client-app/", // ASP.NET Core public folder
+    filenameHashing: false // , // I'll let ASP.NET Core handle cache breaking
+    //publicPath: "/client-app/" // almappa esetén be kell állítani?
 };
