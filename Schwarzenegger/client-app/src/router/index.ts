e62bb8c103ce6b4982ff27b0036d0ba48c0efdf9
László Szücs <laszlo.szucs@euroknows.com>
2020-03-11 13:11:46 +0100

    route bugfix

--- Schwarzenegger/client-app/src/router/index.ts
+++ Schwarzenegger/client-app/src/router/index.ts
@@ -29,7 +29,7 @@ const router = new VueRouter({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "../views/About.vue")
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     },
     {
       path: "/account",
